24f94cc42b69439b16e03c4125f13a864a29f9aa
kunal <yuvrajsinghthapa609@gmail.com>
2023-07-24 02:33:53 +0530

    Updated code with fixes

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,29 @@ class BackgroundHelloApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(
+          title: const Text('EVOLKUN'),
+          backgroundColor: Colors.transparent,
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color.fromARGB(255, 2, 47, 84),
+                  Colors.green,
+                  Color.fromARGB(255, 139, 18, 18)
+                ], // Define your gradient colors here
+              ),
+            ),
+          ),
+        ),
         body: Stack(
           children: [
             Image.asset(
-              'assets/images/ytbg.jpg',
+              'assets/images/ytbg2.jpeg',
               fit: BoxFit.cover,
               height: double.infinity,
               width: double.infinity,
